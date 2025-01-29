cc8a07b259a4ec7877a47e2a40bce8c2dcea7cc1
beatrisem <49399314+beatrisem@users.noreply.github.com>
2025-01-29 20:54:43 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -20,12 +20,14 @@ def get_request(endpoint, **kwargs):
             params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
-
+    
     print(f"GET from {request_url}")
+    
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
+
     except requests.exceptions.RequestException as err:
         # If any error occurs
         print(f"Network exception occurred: {err}")
@@ -35,21 +37,23 @@ def get_request(endpoint, **kwargs):
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
+    
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
+
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
-
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
+    
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    
+    except Exception as e:
+        print(f"Error: {e}")
