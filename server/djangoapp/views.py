760874e09c3d8751ecb8734f7c475ced5691631c
beatrisem <beatrisemazjane@yahoo.com>
2025-01-30 09:59:24 -0500

    module 5

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -16,7 +16,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments
-# from .restapis import post_review
+from .restapis import post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -51,7 +51,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    # context = {}
+    context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -60,7 +60,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    # email_exist = False
+    email_exist = False
 
     try:
         # Check if user already exists
@@ -131,18 +131,19 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 def add_review(request):
-    if not request.user.is_anonymous:
-        # data = json.loads(request.body)
+    if (request.user.is_anonymous is False):
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
-        except Exception as e:
-            return JsonResponse({
-                "status": 401,
-                "message": f"Error in posting review: {str(e)}"
-            })
+        except:
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
 
 
 # def get_cars(request):
